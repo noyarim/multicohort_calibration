c0469f00fced549f924b2e83e7f42a9a8946de7c
Fernando Alarid-Escudero <feralaes@users.noreply.github.com>
2022-05-23 17:55:26 -0500

    Update 02_decision_model_functions.R

--- R/02_decision_model_functions.R
+++ R/02_decision_model_functions.R
@@ -1,15 +1,15 @@
 #' Transition probability (TP) array for alcohol model
 #' 
-#' \code{create_tp} generates the transition probability array for the CBM model.
+#' \code{create_tpm} generates the transition probability array for the CBM model.
 #'
 #' @param l_params_all List with all parameters of decision model
 #' @param err_stop Logical variable to stop model run if set up as TRUE. Default = FALSE.
 #' @param verbose Logical variable to indicate print out of messages. Default = FALSE
 #' @param Trt is this the Treat All strategy? (default is FALSE)
 #' @return 
-#' The transition probability array for the CBM model.
+#' The transition probability array for the alcohol model.
 #' @export
-create_tp <- function(l_params_all, err_stop = FALSE, verbose = FALSE, 
+create_tpm <- function(l_params_all, err_stop = FALSE, verbose = FALSE, 
                    Trt = FALSE
 ){
   ### Depends on:
@@ -28,14 +28,23 @@ create_tp <- function(l_params_all, err_stop = FALSE, verbose = FALSE,
     
     # From ND ----
     ## to CD (adjusted by treatment status) ----
+    v_r_ND_CD <- c(rep(r_ND_CD_5, 5), )
     v_r_ND_CD_Rx <- v_r_ND_CD*(1-Trt) + # No treatment
       v_r_ND_CD*hr_ND_CD_Rx*(Trt)     # Treatment
-    # From CD to FD
+    ## To Death ----
+    v_r_ND_Death <- 
+      
+    # From CD  ----
+    ## to FD ----
     v_r_CD_FD <- 
-    # From FD to CD
-    v_r_FD_CD <- 
-    # From FD to CD
+    ## To Death ----
     
+    # From FD  ----
+    ## to CD ----
+    v_r_FD_CD <- 
+    ## To Death ----
+    v_r_FD_Death <- 
+  
     ## Transition Intensity Array -------------------------------------------
     a_Q <- array(0, dim = list(n_states, n_states, n_cycles), 
                  dimnames = list(v_names_states, v_names_states, v_age_names))
@@ -51,21 +60,23 @@ create_tp <- function(l_params_all, err_stop = FALSE, verbose = FALSE,
     a_Q["FD", "CD", ]   <- 
     a_Q["FD", "Dead", ] <- 
     
-    ### Compute Transition proability 
+    ### Compute transition probability array
     a_P <- sapply(seq_along(v_age_names), 
                   function(x) msm::MatrixExp(mat = a_G[,,x], t = t_scale), 
                   simplify="array")
     
       
     ## Check if Transition Probability array is valid
-    check_transition_probability(a_P, err_stop = err_stop, verbose = verbose)
-    check_sum_of_transition_array(a_P, n_states = n_states, n_t = n_t,
-                                  err_stop = err_stop, verbose = verbose)
-    
-    return(a_P) # Return transition probability array
+    # darthtools::check_transition_probability(a_P, err_stop = err_stop, 
+    #                                          verbose = verbose)
+    # darthtools::check_sum_of_transition_array(a_P, n_states = n_states, 
+    #                                           n_cycles = n_cycles,
+    #                                           err_stop = err_stop, 
+    #                                           verbose = verbose)
     
     # Return transition probability array -------------------------------------
     return(a_P) 
   }
   )
 }
+
