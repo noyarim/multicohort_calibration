bc36854c27103d0bbd1ea45d392a4034f52cc0ec
noyarim <KEL171@pitt.edu>
2022-05-25 14:25:32 -0400

    Create 03_alcohol_model_functions.R

---
+++ R/03_alcohol_model_functions.R
@@ -0,0 +1,33 @@
+#' 4-state alcohol model
+#' 
+#' \code{create_tpm} generates the transition probability array for the CBM model.
+#'
+#' @param a_P Array of age-specific transition matrices. 
+#' @param err_stop Logical variable to stop model run if set up as TRUE. Default = FALSE.
+#' @param verbose Logical variable to indicate print out of messages. Default = FALSE
+#' @param Trt is this the Treat All strategy? (default is FALSE)
+#' @return 
+#' The markov trace for the alcohol model. 
+#' @export
+run_drinkmc <- function(l_param_all, a_P, err_stop = FALSE, verbose = FALSE, 
+                       Trt = FALSE
+){
+  ### Depends on:
+  ###   - Package `msm` to compute the matrix exponential
+  with(as.list(l_params_all), {
+    # Sanity Check
+    if (n_cycles_year <= 0) {
+      stop("Each year must have at least one cycle")
+    }
+    # Discrete-time alcohol model -----------------------------------------------------
+    mc_trace <- matrix(0,nrow = n_cycles_year, ncol=n_states) # Markov trace matrix
+    
+    for (t in n_cycles_year){
+      mc_trace[t+1,] <- mc_trace[t,] %*% a_P[,,t]
+    }
+
+    # Return markov trace matrix -------------------------------------
+    return(mc_trace) 
+  }
+  )
+}
