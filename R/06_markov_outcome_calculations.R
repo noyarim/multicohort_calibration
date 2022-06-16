69e66d5daafb2754f87596e01e3ab76e4e9eb913
Kyueun Lee <noyarim@gmail.com>
2022-06-16 13:04:22 -0400

    Update 06_markov_outcome_calculations.R
    Added prevalence of former, and never heavy drinkers to the list of model outcomes.

--- R/06_markov_outcome_calculations.R
+++ R/06_markov_outcome_calculations.R
@@ -7,25 +7,38 @@
 #' @export
 cal_prev <- function(mc_trace){
   # Calculate prevalence of current drinkers
-  prev_all <- mc_trace[,2] / (1-mc_trace[,ncol(mc_trace)])
+  prev_CD <- mc_trace[,2] / (1-mc_trace[,ncol(mc_trace)])
+  prev_ND <- mc_trace[,1] / (1-mc_trace[,ncol(mc_trace)])
+  prev_FD <- mc_trace[,3] / (1-mc_trace[,ncol(mc_trace)])
   # Group into 4-year age buckets
   age_groups <- c("16-19","20-23","24-27","28-31","32-35",
                   "36-40","44-47","48-51","52-55","56-59",
                   "60-63","64-67","68-71","72-")
   n_agegroups = length(age_groups)
-  prev_bygroup <- data.frame(Age=age_groups,
+  prev_CD_bygroup <- data.frame(Age=age_groups,
                             Prev=rep(0,n_agegroups))
+  prev_ND_bygroup <- data.frame(Age=age_groups,
+                                Prev=rep(0,n_agegroups))
+  prev_FD_bygroup <- data.frame(Age=age_groups,
+                                Prev=rep(0,n_agegroups))
   # Calculate the average prevalence by age groups
   for (i in 1:n_agegroups){
     if (i < n_agegroups){
-      prev_bygroup$Prev[i] <- mean(prev_all[(4*(i-1)+1):(4*i)])
+      prev_CD_bygroup$Prev[i] <- mean(prev_CD[(4*(i-1)+1):(4*i)])
+      prev_ND_bygroup$Prev[i] <- mean(prev_ND[(4*(i-1)+1):(4*i)])
+      prev_FD_bygroup$Prev[i] <- mean(prev_FD[(4*(i-1)+1):(4*i)])
     }else{
       # age group older than 72
-      prev_bygroup$Prev[i] <- mean(prev_all[(4*(i-1)+1):nrow(mc_trace)])
+      prev_CD_bygroup$Prev[i] <- mean(prev_CD[(4*(i-1)+1):nrow(mc_trace)])
+      prev_ND_bygroup$Prev[i] <- mean(prev_ND[(4*(i-1)+1):nrow(mc_trace)])
+      prev_FD_bygroup$Prev[i] <- mean(prev_FD[(4*(i-1)+1):nrow(mc_trace)])
     }
   }
   
-  return(prev_bygroup)
+  model.res <- list(Prev_CD = prev_CD_bygroup,
+                    Prev_ND = prev_ND_bygroup,
+                    Prev_FD = prev_FD_bygroup)
+  return(model.res)
   
 }
 
